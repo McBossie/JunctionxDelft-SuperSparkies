783a8deae0667def2b7c3e0cdd1cd3f690e208fd
ahmobeka <ahmed.raif2006@gmail.com>
2025-10-04 13:24:54 +0200

    ..

--- ahm.py
+++ ahm.py
@@ -1,4 +1,15 @@
 import pandas as pd
 
-def data_sheet(data):
-    pd.read_excel()
+def load_excel_data(file_path):
+
+  df = pd.read_excel(file_path)
+  return df
+
+path_to_file = "data/data_sets.xlsx" 
+
+my_uber_data = load_excel_data(path_to_file)
+
+print(my_uber_data.head())
+
+# in terminal : pip install pandas
+#               pip install openpyxl
